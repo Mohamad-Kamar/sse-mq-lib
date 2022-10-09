42cc6c014f5fc3b0ab1de15b16e534ca66993a2a
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-10-10 00:14:17 +0300

    feat: add producer and consumer classes

---
+++ Consumer.js
@@ -0,0 +1,74 @@
+const httpFetcher = require("./HttpFetcher");
+
+const uid = () => String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, '');
+
+class Consumer {
+    constructor(url) {
+        this.url = url;
+        this.queueName = '';
+        this.queueType = null;
+        this.event = null;
+    }
+
+    connect({ queueName, queueType }) {
+        this._resetParams({ queueName, queueType });
+        let connectObj = this._getCreateObj();
+        let connectionSearchParamsString = new URLSearchParams(connectObj).toString();
+        let urlWithParams = `${this.url}/consumer/connect?${connectionSearchParamsString}`;
+        let event = new EventSource(urlWithParams);
+        this.event = event;
+        this.event.onmessage = this.onmessage;
+        this.event.onerror = this.onerror;
+    }
+
+    onmessage(message) {
+        console.log(message);
+    }
+    setOnMessage(onmessage) {
+       this.event.onmessage = onmessage;
+    }
+    onerror(message) {
+        console.error(message);
+    }
+    setOnerror(onerror) {
+        this.event.onerror = onerror;
+    }
+
+    async craete({ queueName, queueType }) {
+        this._resetParams({ queueName, queueType });
+
+        let createObj = this._getCreateObj();
+        let r = httpFetcher({
+            hostname: `${this.url}/queue`,
+            method: 'POST',
+            body: JSON.stringify(createObj),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        let jsonedResults = await r.json();
+        return jsonedResults.connectionParams;
+    }
+
+    _getCreateObj() {
+        let createObj = {};
+        if(this.queueName) createObj.queueName = this.queueName;
+        if(this.queueType) createObj.queueType = this.queueType;
+        return createObj;
+    }
+
+    _resetParams({ queueName, queueType }) {
+        this._resetEvent();
+
+        queueName ? this.queueName = queueName : this.queueName = uid();
+        queueType ? this.queueType = queueType : this.queueType = null;
+    }
+
+    _resetEvent() {
+        this.event && this.event.close();
+        this.event = null;
+    }
+}
+
+module.exports = Consumer
+
---
+++ Producer.js
@@ -0,0 +1,61 @@
+const httpFetcher = require("./HttpFetcher");
+
+class Producer{
+    constructor(url, queueName='', queueType=null){
+        this.url = url;
+        this.queueName = '';
+        this.queueType = null;
+    }
+
+    publish({queueName, queueType, message}){
+        this._setParams({queueName, queueType})
+        let createObj = this._getCreateObj();
+        let messageObj = this._getMessageObj(createObj);
+        let r = httpFetcher({
+            hostname: `${this.url}/producer`,
+            method: 'POST',
+            body: JSON.stringify(messageObj),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return true;
+    }
+
+    async craete({queueName, queueType}){
+        this._resetParams({queueName, queueType});
+
+        let createObj = this._getCreateObj();
+        let r = httpFetcher({
+            hostname: `${this.url}/queue`,
+            method: 'POST',
+            body: JSON.stringify(createObj),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        let jsonedResults = await r.json();
+        return jsonedResults.connectionParams;
+    }
+
+    _getCreateObj(){
+        let createObj = {};
+        if(this.queueName) createObj.queueName = this.queueName;
+        if(this.queueType) createObj.queueType = this.queueType;
+        return createObj;
+    }
+    _getMessageObj(createObj){
+        return {...createObj, message}
+    }
+
+    _resetParams({queueName, queueType}){
+        queueName ? this.queueName = queueName: this.queueName = uid();
+        queueType ? this.queueType = queueType: this.queueType = null;
+    }
+    _setParams({queueName, queueType}){
+        if(queueName) this.queueName = queueName;
+        if(queueType) this.queueType = queueType;
+    }
+}
+
+module.exports = Producer
