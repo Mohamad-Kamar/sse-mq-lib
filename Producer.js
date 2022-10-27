f435959a906ede7a7dad0c87155e68a5a6dd491a
Mohamad Kamar <mohamad.kamar@postlight.com>
2022-10-27 15:41:43 +0300

    feat: improve producer class

--- Producer.js
+++ Producer.js
@@ -1,61 +1,35 @@
 const fetch = require('cross-fetch');
 
-class Producer{
-    constructor(url, queueKey='', queueType=null){
-        this.url = url;
-        this.queueKey = '';
-        this.queueType = null;
-    }
+class Producer {
+  constructor(url, { queueKey }) {
+    this.url = url;
+    this.queueKey = queueKey;
+  }
 
-    publish({queueKey, queueType, message}){
-        this._setParams({queueKey, queueType})
-        let createObj = this._getCreateObj();
-        let messageObj = this._getMessageObj(createObj);
-        let r = fetch({
-            hostname: `${this.url}/producer`,
-            method: 'POST',
-            body: JSON.stringify(messageObj),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        return true;
-    }
+  async publish(message, { queueKey }) {
+    this.setParams({ queueKey });
+    const messageObj = this.getCreateMessageObj(message);
+    const results = await fetch({
+      hostname: `${this.url}/producer`,
+      method: 'POST',
+      body: JSON.stringify(messageObj),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    return results.status === 201;
+  }
 
-    async craete({queueKey, queueType}){
-        this._resetParams({queueKey, queueType});
+  getCreateMessageObj(message) {
+    const createObj = {};
+    if (this.queueKey) createObj.queueKey = this.queueKey;
+    createObj.message = message;
+    return createObj;
+  }
 
-        let createObj = this._getCreateObj();
-        let r = fetch({
-            hostname: `${this.url}/queue`,
-            method: 'POST',
-            body: JSON.stringify(createObj),
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-        let jsonedResults = await r.json();
-        return jsonedResults.connectionParams;
-    }
-
-    _getCreateObj(){
-        let createObj = {};
-        if(this.queueKey) createObj.queueKey = this.queueKey;
-        if(this.queueType) createObj.queueType = this.queueType;
-        return createObj;
-    }
-    _getMessageObj(createObj){
-        return {...createObj, message}
-    }
-
-    _resetParams({queueKey, queueType}){
-        queueKey ? this.queueKey = queueKey: this.queueKey = uid();
-        queueType ? this.queueType = queueType: this.queueType = null;
-    }
-    _setParams({queueKey, queueType}){
-        if(queueKey) this.queueKey = queueKey;
-        if(queueType) this.queueType = queueType;
-    }
+  setParams({ queueKey }) {
+    if (queueKey) this.queueKey = queueKey;
+  }
 }
 
-module.exports = Producer
+module.exports = Producer;
